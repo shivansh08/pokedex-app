72ad1bd143ccf1382a96d6f655927bf0648a96e7
shivansh08 <78873044+shivansh08@users.noreply.github.com>
2022-05-05 02:12:45 +0530

    Update Card.jsx

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,36 +1,6 @@
 import React from "react";
-// import { useState,useEffect } from "react"; 
 
 const Card=({pokemon,loading,infoPokemon})=>{
-
-    // const [users, setUsers] = useState([])
-    
-    // const [pokeData, setPokeData]=useState([]);
-    
-    // const fetchData = async () => {
-    //     const data = await (await fetch("https://pokeapi.co/api/v2/pokemon/")).json()
-    //     setUsers(data["results"])
-    // }
-
-    // const getPokemon=async()=>{
-    //     const res= await (await fetch("https://pokeapi.co/api/v2/pokemon/")).json();
-    //     const result= res.results
-    //     result.map(async(item)=>{
-    //         const result=await (await fetch(item.url)).json()
-    //         //console.log(result.data)
-    //         setPokeData(state=>{
-    //             state=[...state,result]
-    //             state.sort((a,b)=>a.id>b.id?1:-1)
-    //             return state;
-    //         })
-    //     })
-    // }
-    
-    // useEffect(() => {
-    //     getPokemon()
-    // }, [])
-    
-    
     return(
         <>
             {
